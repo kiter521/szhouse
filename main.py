745827c6cd91e4cc3faae805a000995666517aa1
huxiaokan <kiter521@163.com>
2018-12-03 19:58:31 +0800

    add main.py file for master

---
+++ main.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python
+# -*- coding: utf-8-*-
+# pylint:disable=invalid-name
+
+"""Here is import lib
+"""
+import requests
+#from bs4 import BeautifulSoup
+
+"""Execute SQL on multiple servers
+"""
+if __name__ == "__main__":
+    print "My first python program"
